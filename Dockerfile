2d3328b5557ab687e7b0254ef7d48b3d86030628
Tjonk <tjonk11@gmail.com>
2024-03-01 17:18:44 +0100

    Update from complete project.

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,29 @@
-# Load Python 3.8 dependencies using slim Debian 10 image.
 FROM python:3.8-slim-buster
 
-# Build variables.
 ENV DEBIAN_FRONTEND noninteractive
+RUN apt-get update -y \
+    && apt-get install -y --no-install-recommends \
+        curl \
+        gcc \
+        g++ \
+        gnupg \
+        unixodbc-dev \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
 
-# Install Microsoft SQL Server requirements.
 ENV ACCEPT_EULA=Y
-RUN apt-get update -y && apt-get update \
-  && apt-get install -y --no-install-recommends curl gcc g++ gnupg unixodbc-dev
-
-# Add SQL Server ODBC Driver 18 for Debian 10
 RUN curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - \
-  && curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list \
-  && apt-get update \
-  && apt-get install -y --no-install-recommends --allow-unauthenticated msodbcsql18 mssql-tools \
-  && echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile \
-  && echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+    && curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list \
+    && apt-get update \
+    && apt-get install -y --no-install-recommends --allow-unauthenticated \
+        msodbcsql18 \
+        mssql-tools \
+    && echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 
 
 WORKDIR /app
 COPY . /app
 RUN pip install --no-cache-dir -r requirements.txt
 ENV FLASK_APP=main.py
-EXPOSE 3000
-CMD ["flask", "run", "--host=0.0.0.0", "--port=3000"]
+# EXPOSE 5000
+CMD ["flask", "run", "--host=0.0.0.0", "--port=5000"]
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sqlite3
 import secrets
 import pyodbc
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, url_for, redirect, session
 from flask_bcrypt import Bcrypt
 
@@ -15,12 +17,43 @@ connection = sqlite3.connect(DATABASE, check_same_thread=False)
 cursor = connection.cursor()
 '''
 
-connection_string = ('Driver={ODBC Driver 18 for SQL Server};Server=tcp:cloudedgeassignment.database.windows.net,1433;'
-                     'Database=CloudEdgeAssignment;Uid=CloudAdmin;Pwd={kyh2023!};Encrypt=yes;'
-                     'TrustServerCertificate=no;Connection Timeout=30;')
+load_dotenv()
+database_driver = os.getenv('DATABASE_DRIVER')
+database_server = os.getenv('DATABASE_SERVER')
+database_name = os.getenv('DATABASE_NAME')
+database_user = os.getenv('DATABASE_USER')
+database_password = os.getenv('DATABASE_PASSWORD')
+
+connection_string = (
+    f'Driver={{{database_driver}}};'
+    f'Server={{{database_server}}};'
+    f'Database={{{database_name}}};'
+    f'Uid={{{database_user}}};'
+    f'Pwd={{{database_password}}};'
+    'Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+)
 connection = pyodbc.connect(connection_string)
 cursor = connection.cursor()
 
+
+
+def get_all_movies():
+    cursor.execute("SELECT title, rating, user_id FROM movies")
+    movie_list_db = cursor.fetchall()
+    movie_list = add_all_movies_to_dict(movie_list_db)
+    return movie_list
+
+
+def add_all_movies_to_dict(movies_list):
+    temp_movies = []
+    for movie in movies_list:
+        title, rating, userid = movie
+        cursor.execute("SELECT user_name FROM users WHERE user_id=?", (userid,))
+        user_data = cursor.fetchone()
+        username = user_data[0]
+        movie_to_add = {"title": title, "rating": rating, "username": username}
+        temp_movies.append(movie_to_add)
+    return temp_movies
 
 
 def get_movies(user_id):
@@ -41,26 +74,46 @@ def add_movies_to_dict(movies_list):
 
 @app.route("/")
 def index():
-    return render_template('welcome.html')
+    return render_template("welcome.html")
 
 
 @app.route("/movies", methods=["GET"])
 def movies():
     userid = session["userid"]
     movie_list = get_movies(userid)
-    return render_template('movies.html', movie_list=movie_list)
+    all_movies_list = get_all_movies()
+
+    success_message = request.args.get('success_message')
+    error_message = request.args.get('error_message')
+    return render_template('movies.html',
+                           movie_list=movie_list,
+                           all_movies_list=all_movies_list,
+                           sucess_message=success_message,
+                           error_message=error_message)
 
 
 @app.route("/add_movies", methods=["POST"])
 def add_movies():
     movie_title = request.form["title"]
     movie_rating = request.form["rating"]
+    movie_rating = int(movie_rating)
+    if movie_rating > 100:
+        movie_rating = 100
+    elif movie_rating < 1:
+        movie_rating = 1
     movie_info = request.form["info"]
     user_id = session["userid"]
-    new_movie = "INSERT INTO movies (title, rating, info, user_id) VALUES (?, ?, ?, ?)"
-    cursor.execute(new_movie, (movie_title, movie_rating, movie_info, user_id))
-    connection.commit()
-    return redirect(url_for('movies'))
+
+    try:
+        new_movie = "INSERT INTO movies (title, rating, info, user_id) VALUES (?, ?, ?, ?)"
+        cursor.execute(new_movie, (movie_title, movie_rating, movie_info, user_id))
+        connection.commit()
+        success_message = "Added movie."
+        return redirect(url_for("movies") + "?success_message=" + success_message)
+
+    except sqlite3.IntegrityError:
+        error_message = "Title already exists."
+        return redirect(url_for("movies") + "?error_message=" + error_message)
 
 
 @app.route("/remove_movie/<movie>", methods=["GET"])
@@ -68,13 +121,13 @@ def remove_movies(movie):
     userid = session["userid"]
     cursor.execute("DELETE FROM movies WHERE user_id=? AND title=?", (userid, movie))
     connection.commit()
-    return redirect(url_for('movies'))
+    return redirect(url_for("movies"))
 
 
 @app.route("/description", methods=["GET"])
 def description():
     movie_list = get_movies(session["userid"])
-    movie_name = request.args.get('movie')
+    movie_name = request.args.get("movie")
     info = None
     rating = None
 
@@ -102,7 +155,7 @@ def login():
             existing_user = cursor.fetchone()
             if existing_user is not None:
                 session["userid"] = existing_user[0]
-                return redirect(url_for('movies'))
+                return redirect(url_for("movies"))
         else:
             error_message = "Wrong password."
             return render_template("welcome.html", error_message=error_message)
@@ -113,7 +166,6 @@ def login():
 
 @app.route("/logout", methods=["GET"])
 def logout():
-    connection.close()
     return render_template("welcome.html")
 
 
@@ -125,20 +177,20 @@ def register_page():
 @app.route("/register_user", methods=["POST"])
 def register():
     username = request.form["username"]
-    error_message = None
 
     if not username.isalnum():
         error_message = "Invalid username."
         return render_template("register.html", error_message=error_message)
     password = request.form["password"]
-    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
 
     try:
         new_user = "INSERT INTO users (user_name, password) VALUES (?, ?)"
         cursor.execute(new_user, (username, hashed_password))
         connection.commit()
         success_message = "Registration successful"
-    except sqlite3.IntegrityError:
+        error_message = None
+    except pyodbc.IntegrityError:
         error_message = "Username already exists."
         success_message = None
 
@@ -148,5 +200,5 @@ def register():
         return render_template("register.html", error_message=error_message)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
